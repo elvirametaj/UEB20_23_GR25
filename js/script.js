35175b371c37631560dcffe42e0931e89bb70efe
ViolaVeselaj <viole.veselaj@student.uni-pr.edu>
2024-01-05 22:37:05 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -32,3 +32,25 @@ videoBtn.forEach(btn => {
   });
 
 
+/* description: send email to concat*/
+/* @param concat form : object*/
+/* return: boolean -*/
+
+function sendMail(contactForm) {
+  emailjs.send("default_service", "travel", {
+      "from_name": contactForm.name.value,
+      "from_email": contactForm.emailaddress.value,
+      "packet_information": contactForm.packetinformation.value
+  })
+  .then(
+      function(response) {
+          console.log("SUCCESS", response);
+      },
+      function(error) {
+          console.log("FAILED", error);
+      }
+  );
+  return false;  
+}
+
+
