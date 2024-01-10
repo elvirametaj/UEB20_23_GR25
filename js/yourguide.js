556bab858c69678d4463223e99c8e5f4367f5d88
edisonhajdari <ediisonhajdari@gmail.com>
2024-01-10 22:00:50 +0100

    Update Your Guide

--- js/yourguide.js
+++ js/yourguide.js
@@ -58,17 +58,16 @@ var countryData = {
 
         ]
     },
-    poland: {
-        name: "Poland",
-        description: "Poland, a country nestled in the heart of Europe...",
-        price: "$700",
-        imagescr: "/images/polandbgimage.jpg",
-        shortdescription:"Poland, a country adorned with breathtaking natural beauty, captivates visitors with its diverse landscapes and enchanting vistas. From the majestic Tatra Mountains in the south, where pristine alpine lakes mirror the towering peaks, to the picturesque Masurian Lakes region in the north, Poland offers a kaleidoscope of natural wonders Lush green forests blanket vast expanses of the countryside, revealing hidden gems like the Białowieża Forest, a UNESCO World Heritage site and Europe's last primeval forest. The Baltic Sea coastline introduces a different charm, with sandy beaches and serene coastal dunes stretching as far as the eye can see Polands numerous national parks, such as the Bieszczady and Karkonosze, showcase the countrys commitment to preserving its rich biodiversity. Tranquil lakes, meandering rivers, and rolling hills contribute to the countrys idyllic charm, inviting travelers to explore its untouched landscapes.",
-
+    albania: {
+        name: "Albania",
+        description: "Albania, a country nestled in the heart of Europe...",
+        price: "$500",
+        imagescr: "/images/albaniabg.jpeg",
+        shortdescription:"Albania, a hidden gem in the heart of the Balkans, unveils a tapestry of natural wonders that captivate the soul. The pristine beaches along the Albanian Riviera, kissed by the turquoise waters of the Adriatic and Ionian Seas, invite relaxation in tranquil coastal settings. Inland, the rugged landscapes of the Albanian Alps boast majestic peaks and pristine alpine lakes, offering a haven for hikers and nature enthusiasts. The tranquil shores of Lake Ohrid, a UNESCO World Heritage site, reflect the beauty of medieval towns nestled along its banks. Albania's diverse ecosystems include lush forests, such as the protected areas of Llogara Pass and Valbona Valley, where biodiversity thrives. From the historic landscapes of Butrint National Park to the untamed beauty of Theth National Park, Albania beckons travelers to discover its rich tapestry of natural wonders",
         images: [
-            '/images/poland.jpg',
-            '/images/poland2.webp',
-            '/images/nature-1.jpg',
+            '/images/albania1.webp',
+            '/images/albania2.jpeg',
+            '/images/albania3.jpeg',
 
 
         ]
