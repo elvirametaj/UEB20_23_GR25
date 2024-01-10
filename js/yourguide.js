76509720098126b7ea39b92346cc1e9e990e3966
edisonhajdari <ediisonhajdari@gmail.com>
2024-01-10 21:55:11 +0100

    Update Your Guide

--- js/yourguide.js
+++ js/yourguide.js
@@ -14,6 +14,65 @@ var countryData = {
 
         ]
     },
+    portugal: {
+        name: "Portugal",
+        description: "Portugal, a country of diversity...",
+        price: "$800",
+        imagescr: "/images/portugalbg.jpg",
+        shortdescription:"Portugal, a country blessed with enchanting landscapes, captivates visitors with its diverse natural beauty. From the golden beaches of the Algarve to the lush vineyards of the Douro Valley, Portugal offers a tapestry of scenic wonders. The rugged coastline is adorned with dramatic cliffs and hidden coves, providing breathtaking views of the Atlantic Ocean. Inland, rolling hills are dotted with picturesque villages and olive groves, while the majestic Serra da Estrela mountain range invites exploration with its pristine lakes and alpine landscapes. Portugal's natural beauty extends to its national parks, such as Peneda-Gerês, where dense forests, cascading waterfalls, and wildlife abound. Whether wandering through ancient cork oak forests or savoring the tranquility of the Azores' volcanic landscapes, Portugal invites travelers to immerse themselves in a world of captivating natural wonders.",
+        images: [
+            '/images/portugal1.jpg',
+            '/images/portugal2.jpeg',
+            '/images/portugal3.jpeg',
+
+
+        ]
+    },
+    poland: {
+        name: "Poland",
+        description: "Poland, a country nestled in the heart of Europe...",
+        price: "$700",
+        imagescr: "/images/polandbgimage.jpg",
+        shortdescription:"Poland, a country adorned with breathtaking natural beauty, captivates visitors with its diverse landscapes and enchanting vistas. From the majestic Tatra Mountains in the south, where pristine alpine lakes mirror the towering peaks, to the picturesque Masurian Lakes region in the north, Poland offers a kaleidoscope of natural wonders Lush green forests blanket vast expanses of the countryside, revealing hidden gems like the Białowieża Forest, a UNESCO World Heritage site and Europe's last primeval forest. The Baltic Sea coastline introduces a different charm, with sandy beaches and serene coastal dunes stretching as far as the eye can see Polands numerous national parks, such as the Bieszczady and Karkonosze, showcase the countrys commitment to preserving its rich biodiversity. Tranquil lakes, meandering rivers, and rolling hills contribute to the countrys idyllic charm, inviting travelers to explore its untouched landscapes.",
+
+        images: [
+            '/images/poland.jpg',
+            '/images/poland2.webp',
+            '/images/nature-1.jpg',
+
+
+        ]
+    },
+    poland: {
+        name: "Poland",
+        description: "Poland, a country nestled in the heart of Europe...",
+        price: "$700",
+        imagescr: "/images/polandbgimage.jpg",
+        shortdescription:"Poland, a country adorned with breathtaking natural beauty, captivates visitors with its diverse landscapes and enchanting vistas. From the majestic Tatra Mountains in the south, where pristine alpine lakes mirror the towering peaks, to the picturesque Masurian Lakes region in the north, Poland offers a kaleidoscope of natural wonders Lush green forests blanket vast expanses of the countryside, revealing hidden gems like the Białowieża Forest, a UNESCO World Heritage site and Europe's last primeval forest. The Baltic Sea coastline introduces a different charm, with sandy beaches and serene coastal dunes stretching as far as the eye can see Polands numerous national parks, such as the Bieszczady and Karkonosze, showcase the countrys commitment to preserving its rich biodiversity. Tranquil lakes, meandering rivers, and rolling hills contribute to the countrys idyllic charm, inviting travelers to explore its untouched landscapes.",
+
+        images: [
+            '/images/poland.jpg',
+            '/images/poland2.webp',
+            '/images/nature-1.jpg',
+
+
+        ]
+    },
+    poland: {
+        name: "Poland",
+        description: "Poland, a country nestled in the heart of Europe...",
+        price: "$700",
+        imagescr: "/images/polandbgimage.jpg",
+        shortdescription:"Poland, a country adorned with breathtaking natural beauty, captivates visitors with its diverse landscapes and enchanting vistas. From the majestic Tatra Mountains in the south, where pristine alpine lakes mirror the towering peaks, to the picturesque Masurian Lakes region in the north, Poland offers a kaleidoscope of natural wonders Lush green forests blanket vast expanses of the countryside, revealing hidden gems like the Białowieża Forest, a UNESCO World Heritage site and Europe's last primeval forest. The Baltic Sea coastline introduces a different charm, with sandy beaches and serene coastal dunes stretching as far as the eye can see Polands numerous national parks, such as the Bieszczady and Karkonosze, showcase the countrys commitment to preserving its rich biodiversity. Tranquil lakes, meandering rivers, and rolling hills contribute to the countrys idyllic charm, inviting travelers to explore its untouched landscapes.",
+
+        images: [
+            '/images/poland.jpg',
+            '/images/poland2.webp',
+            '/images/nature-1.jpg',
+
+
+        ]
+    },
 };
 
 function getParameterByName(name, url) {
@@ -64,21 +123,58 @@ window.addEventListener('load', function () {
             case 'poland':
                 // Your HTML for Poland specific details
                 var polandDetailsHtml = `
-                <div class="card" style="width=100%;">
-                <div class="card-header">
-                  Guide Plan
-                </div>
-                <div class="card-body">
-                  <h5 class="card-title">Guide Plan </h5>
-                  <p class="card-text">Visit Ploand starting from $700</p>
-                  <a onclick="bookNow('Poland' , '700.00')" class="btn btn-primary">Book Now</a>
-                </div>
-              </div>
+                <table class="table table-bordered" >
+                <thead style="background-color:lightblue;color:white;">
+                    <tr>
+                        <th>Category</th>
+                        <th>Medium Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Hotel</td>
+                        <td>$100 - $200 per night</td>
+                    </tr>
+                    <tr>
+                        <td>Restaurant</td>
+                        <td>$15 - $30 per person</td>
+                    </tr>
+                    <!-- Add more rows for other categories -->
+                </tbody>
+            </table>
                 `;
                 var detailsElement = document.createElement('div');
                 detailsElement.innerHTML = polandDetailsHtml;
                 document.body.appendChild(detailsElement);
                 break;
+
+                case 'portugal':
+                    // Your HTML for Poland specific details
+                    var polandDetailsHtml = `
+                    <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Category</th>
+                            <th>Medium Price</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Hotel</td>
+                            <td>$80 - $150 per night</td>
+                        </tr>
+                        <tr>
+                            <td>Restaurant</td>
+                            <td>$12 - $25 per person</td>
+                        </tr>
+                        <tr>
+                            <td>Transportation</td>
+                            <td>$1.50 - $2.50 per ticket</td>
+                        </tr>
+                        <!-- Add more rows for other categories -->
+                    </tbody>
+                </table>
+                    `
         }
     } else {
         countryNameElement.textContent = 'Country information not available.';
