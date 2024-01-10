aed54fd4464255f8996f4a8392b30c027fce2b16
edisonhajdari <ediisonhajdari@gmail.com>
2024-01-10 11:11:19 +0100

    js

--- js/guide.js
+++ js/guide.js
@@ -4,18 +4,21 @@ const locations = [
       tag: 'City',
       name: 'NEW YORK',
       description: 'New York City comprises 5 boroughs sitting where the Hudson River meets the Atlantic Ocean. At its core is Manhattan, a densely populated borough that’s among the world’s major commercial, financial and cultural centers. Its iconic sites include skyscrapers such as the Empire State Building and sprawling Central Park.'
+      ,price:550
     },
     {
       image: '/images/lakeimage.jpg',
       tag: 'Nature',
       name: 'Lake Lucerne',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi perferendis molestiae non nemo doloribus. Doloremque, nihil! At ea atque quidem!'
+      ,price:850
     },
     {
       image: '/images/primaides.jpeg',
       tag: 'Wonders',
       name: 'Egyptian pyramids',
       description: 'The Giza pyramid complex in Egypt is home to the Great Pyramid, the Pyramid of Khafre, and the Pyramid of Menkaure, along with their associated pyramid complexes and the Great Sphinx. All were built during the Fourth Dynasty of the Old Kingdom of ancient Egypt, between c. 2600 – c. 2500 BC.'
+      ,price:650
     }
   ];
 
@@ -44,10 +47,14 @@ const locations = [
     const descriptionParagraph = document.createElement('p');
     descriptionParagraph.textContent = location.description;
 
+    const price = document.createElement('h2');
+    price.textContent = location.price;
+
     cardHeaderDiv.appendChild(image);
     cardBodyDiv.appendChild(tagSpan);
     cardBodyDiv.appendChild(nameHeading);
     cardBodyDiv.appendChild(descriptionParagraph);
+    cardDiv.appendChild(price);
 
     cardDiv.appendChild(cardHeaderDiv);
     cardDiv.appendChild(cardBodyDiv);
