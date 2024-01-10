b9d53d04cfaf0c1f7822b2807483e8d86aa5721b
edisonhajdari <ediisonhajdari@gmail.com>
2024-01-10 20:52:28 +0100

    Update Your Guide

--- js/yourguide.js
+++ js/yourguide.js
@@ -4,8 +4,10 @@ var countryData = {
         description: "Poland, a country nestled in the heart of Europe...",
         price: "$700",
         imagescr: "/images/polandbgimage.jpg",
+        shortdescription:"Poland, a country adorned with breathtaking natural beauty, captivates visitors with its diverse landscapes and enchanting vistas. From the majestic Tatra Mountains in the south, where pristine alpine lakes mirror the towering peaks, to the picturesque Masurian Lakes region in the north, Poland offers a kaleidoscope of natural wonders Lush green forests blanket vast expanses of the countryside, revealing hidden gems like the Białowieża Forest, a UNESCO World Heritage site and Europe's last primeval forest. The Baltic Sea coastline introduces a different charm, with sandy beaches and serene coastal dunes stretching as far as the eye can see Polands numerous national parks, such as the Bieszczady and Karkonosze, showcase the countrys commitment to preserving its rich biodiversity. Tranquil lakes, meandering rivers, and rolling hills contribute to the countrys idyllic charm, inviting travelers to explore its untouched landscapes.",
+
         images: [
-            
+
         ]
     },
 };
