ce629060428c7d8caa87840e68540e2193b14357
edisonhajdari <ediisonhajdari@gmail.com>
2024-01-10 20:07:17 +0100

    new

---
+++ js/yourguide.js
@@ -0,0 +1,9 @@
+var countryData = {
+    poland: {
+        name: "Poland",
+        description: "Poland, a country nestled in the heart of Europe...",
+        price: "$700",  
+      imagescr: "/images/polandgb.jpeg",
+    },
+   
+};
